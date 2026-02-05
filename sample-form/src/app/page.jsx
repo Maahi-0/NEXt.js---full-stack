5d837ee42efebbf516450442fdce3ff52a76743b
Muskan <muskanwagh1608@gmail.com>
2026-02-05 19:08:32 +0530

    new commit with file

---
+++ sample-form/src/app/page.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <div className="text-center px-8">
+        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-600 mb-4">
+          Welcome to Education
+        </h1>
+        <p className="text-lg text-gray-700">Empower your learning journey</p>
+      </div>
+    </div>
+  );
+}
---
+++ sample-form/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+const Navbar = () => {
+    return (
+        <>
+            <nav className="m-0.5shadow-lg">
+                <div className="flex justify-between items-center px-8 py-4 max-w-7xl mx-auto">
+                    <div><Link href="/" className="text-black font-bold text-xl hover:text-gray-300 transition">home</Link></div>
+                    <div className="flex gap-8">
+                        <Link href="/form-filling" className="text-black hover:text-gray-600 transition">Form</Link>
+                        <Link href="/about" className="text-black hover:text-gray-600 transition">About</Link>
+                        <Link href="/contact-us" className="text-black hover:text-gray-600 transition">Contact</Link>
+                    </div>
+                </div>
+            </nav>
+
+        </>
+    );
+}
+export default Navbar;
